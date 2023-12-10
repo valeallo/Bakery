03193893a44adc7523ee27505bf8f3f536fe358f
valeallo <valentina.allodi@gmail.com>
2023-12-10 14:16:11 +0100

    added alt

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -44,7 +44,7 @@ const ProductPage = () => {
               <section class="text-gray-700 body-font overflow-hidden bg-white">
                 <div class="container px-5 py-24 mx-auto">
                   <div class="lg:w-4/5 mx-auto flex flex-wrap">
-                    <img  class="lg:w-1/2 w-full h-30 object-cover object-center rounded border border-gray-200" src={pastry.imageUrl} />
+                    <img  class="lg:w-1/2 w-full h-30 object-cover object-center rounded border border-gray-200" src={pastry.imageUrl}  alt={pastry.name} />
                     <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                       <h2 class="text-sm title-font text-gray-500 tracking-widest">LUANA & MARIA</h2>
                       <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{pastry.name}</h1>
